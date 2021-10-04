7a3ecf1e8b29b03ee1f5534b800d8825d69eeb1a
Saharat Khotnongping <saharatkhotnongping@kkumail.com>
2021-10-05 00:35:42 +0700

    going to sleep.

--- src/main/java/com/training/backend/TestApi/api.java
+++ src/main/java/com/training/backend/TestApi/api.java
@@ -1,20 +1,19 @@
 package com.training.backend.TestApi;
 
 import com.training.backend.business.TestBusiness;
+import com.training.backend.exception.BaseException;
 import com.training.backend.model.MRegisterRequest;
 import com.training.backend.model.TestResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 
 @RestController
 @RequestMapping("/test")
-public class api{
+public class api {
 
     private final TestBusiness business;
+
     public api(TestBusiness business) {
         this.business = business;
     }
@@ -30,14 +29,9 @@ public class api{
 
     @PostMapping
     @RequestMapping("/register")
-    public ResponseEntity<String> register(@RequestBody MRegisterRequest request){
-        String response = null;
-        try {
-            response = business.register(request);
-            return ResponseEntity.ok(response);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).build();
-        }
+    public ResponseEntity<String> register(@RequestBody MRegisterRequest request) throws BaseException {
+        String response = business.register(request);
+        return ResponseEntity.ok(response);
     }
 }
 
--- src/main/java/com/training/backend/business/TestBusiness.java
+++ src/main/java/com/training/backend/business/TestBusiness.java
@@ -1,21 +1,21 @@
 package com.training.backend.business;
 
+import com.training.backend.exception.BaseException;
+import com.training.backend.exception.UserException;
 import com.training.backend.model.MRegisterRequest;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.Objects;
 
 @Service
 public class TestBusiness {
-    public  String register(MRegisterRequest request) throws IOException {
-        if(request == null){
-            throw new IOException("null.request");
+    public String register(MRegisterRequest request) throws BaseException {
+        if (request == null) {
+            throw UserException.requestNull();
         }
 
-        if (Objects.isNull(request.getEmail())){
-            throw new IOException("null.email");
-
+        if (Objects.isNull(request.getEmail())) {
+            throw UserException.emailNull();
         }
 
         return "";
