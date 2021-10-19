e2082e668281a99ce4cfd09c5ed95ddd2ca6a4a1
Saharat Khotnongping <saharatkhotnongping@kkumail.com>
2021-10-19 21:58:37 +0700

    Upload File

--- src/main/java/com/training/backend/TestApi/api.java
+++ src/main/java/com/training/backend/TestApi/api.java
@@ -2,10 +2,12 @@ package com.training.backend.TestApi;
 
 import com.training.backend.business.TestBusiness;
 import com.training.backend.exception.BaseException;
+import com.training.backend.exception.UserException;
 import com.training.backend.model.MRegisterRequest;
 import com.training.backend.model.TestResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -31,6 +33,12 @@ public class api {
     @RequestMapping("/register")
     public ResponseEntity<String> register(@RequestBody MRegisterRequest request) throws BaseException {
         String response = business.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> uploadProfilePicture(@RequestPart MultipartFile file) throws BaseException {
+        String response = business.uploadProfilePicture(file);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/training/backend/business/TestBusiness.java
+++ src/main/java/com/training/backend/business/TestBusiness.java
@@ -1,10 +1,15 @@
 package com.training.backend.business;
 
 import com.training.backend.exception.BaseException;
+import com.training.backend.exception.FileException;
 import com.training.backend.exception.UserException;
 import com.training.backend.model.MRegisterRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -21,4 +26,35 @@ public class TestBusiness {
         return "";
     }
 
+    public String uploadProfilePicture(MultipartFile file) throws UserException {
+        if (file == null) {
+            //throw error
+            throw FileException.fileNull();
+
+        }
+        if (file.getSize() > 1048576 * 2) {
+            //throw error
+            throw FileException.fileMaxSize();
+        }
+
+        String contentType = file.getContentType();
+        if (contentType == null) {
+            //throw error
+            throw FileException.unsupported();
+        }
+
+        List<String> supportedTypes = Arrays.asList("image/jpeg", "image/png");
+        if (supportedTypes.contains(contentType)) {
+            //throw error
+            throw FileException.unsupported();
+        }
+        // TODO: upload file File Storage (AWS, etc.)
+        try {
+            byte[] bytes = file.getBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
 }
